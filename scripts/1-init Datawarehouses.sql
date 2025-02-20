b859c8cd297e72e34aad8995f611f25951f369b1
mohamed ahmed <87114046+mohamedabodonia@users.noreply.github.com>
2025-02-20 16:03:23 +0200

    Add files via upload

---
+++ scripts/1-init Datawarehouses.sql
@@ -0,0 +1,27 @@
+use master;
+Go
+
+
+--Drop the 'DataWarehouse' database if exsist
+
+if exists (select * from sys.databases where name = 'Datawarehouses')
+drop database Datawarehouses
+go
+
+
+-- Create the 'DataWarehouse' database
+create database Datawarehouses;
+go
+
+USE Datawarehouses;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
