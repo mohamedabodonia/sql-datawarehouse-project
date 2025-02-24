daf2744182770bc45cfd15fd8cc88ae94de84209
mohamed ahmed <87114046+mohamedabodonia@users.noreply.github.com>
2025-02-24 19:55:21 +0200

    Update Dimensions & Fact tables in Gold Layer.sql

--- scripts/3-Gold Layer/Dimensions & Fact tables in Gold Layer.sql
+++ scripts/3-Gold Layer/Dimensions & Fact tables in Gold Layer.sql
@@ -65,31 +65,6 @@ GO
 CREATE VIEW gold.fact_sales AS
 SELECT
     sd.sls_ord_num  AS order_number,
-    pr.product_key  AS product_key,
-    cu.customer_key AS customer_key,
-    sd.sls_order_dt AS order_date,
-    sd.sls_ship_dt  AS shipping_date,
-    sd.sls_due_dt   AS due_date,
-    sd.sls_sales    AS sales_amount,
-    sd.sls_quantity AS quantity,
-    sd.sls_price    AS price
-FROM silver.crm_sale sd
-LEFT JOIN gold.dim_products pr
-    ON sd.sls_prd_key = pr.product_number
-LEFT JOIN gold.dim_customers cu
-    ON sd.sls_cust_id = cu.customer_id;
-GO
-
-
--- Create Fact Table: gold.fact_sales
--- =============================================================================
-IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
-    DROP VIEW gold.fact_sales;
-GO
-
-CREATE VIEW gold.fact_sales AS
-SELECT
-    sd.sls_ord_num  AS order_number,
     pr.product_key  AS product_key,    --foregin key
     cu.customer_key AS customer_key,   --foregin key
     sd.sls_order_dt AS order_date,
